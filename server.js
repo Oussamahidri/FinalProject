83fc1a538b56ebea8f8414065a7b7407bb7d3898
oussema hidri <oussama.hidri01@gmail.com>
2023-07-07 14:18:55 +0100

    1.0 project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connect = require('./config/connectDB');
+
+const app = express();
+
+app.use(express.json());
+
+require('dotenv').config()
+
+const PORT = process.env.PORT 
+
+app.listen(PORT,(error)=>{
+    error?console.log(error): console.log(`server is running on ${PORT}`);
+});
+
+connect()
+
+app.use('/api/user',require('./routes/userRoute')) 
+app.use('/api/house',require('./routes/houseRoute')) 
